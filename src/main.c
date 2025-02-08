655ea9609fbc1d4a530265fc18b30d7b397430d3
kareemZafan <kareemm.zafan@gmail.com>
2025-02-08 16:53:17 +0200

    Revert "add my code"
    This reverts commit b77cbc4fc9007fb569b1928c510e26e9a714da0e.

--- src/main.c
+++ src/main.c
@@ -1,15 +1,10 @@
 
 #include<stdio.h>
-a 
-int add(int a , int b){
-return a + b; }
+
 int main(){
 
  printf("Hello World!");
- printf("This is Ahmed's changes");
- // Ahmed Code  
- printf("%d + %d = %d", 5,6, add(5,6)); 
- 
-
+ // Ahmed Code 
+ printf("This is Ahmed's changes"); 
 return 0; 
 }
